a48d184e2d11041eee7255921e4dbe9c1a2a9f20
Kyle Michaels <kyle.michaels@gmail.com>
2024-01-29 17:35:47 -0800

    created and tested number of events component.

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,26 @@
+// src/__tests__/NumberOfEvents.test.js
+
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NumberOfEvents from '../components/NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsComponent;
+  let NOEInput;
+  beforeEach(() => {
+    NumberOfEventsComponent = render(<NumberOfEvents />);
+    NOEInput = NumberOfEventsComponent.queryByRole('textbox');
+  });
+
+  test('renders textbox', () => {
+    expect(NOEInput).toBeInTheDocument();
+  });
+  test('verify that default value is 32', () => {
+    expect(NOEInput).toHaveValue('32');
+  });
+  test('verify that value changes based on user input', async () => {
+    const user = userEvent.setup();
+    await user.type(NOEInput, '{backspace}{backspace}10');
+    expect(NOEInput).toHaveValue('10');
+  });
+});

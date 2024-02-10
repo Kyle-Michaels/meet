1e363b6c81a09980b6732bf1da840e218cfa0027
Kyle Michaels <kyle.michaels@gmail.com>
2024-02-09 18:47:44 -0800

    removed the extra s

--- src/api.js
+++ src/api.js
@@ -70,7 +70,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
-      localStorage.setItems("lastEvents", JSON.stringify(result.events));
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
